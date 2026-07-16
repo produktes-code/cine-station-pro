bde8c7b622abd9b0e7e1488118700e7d127d9533
produktes-code <produktes@gmail.com>
2026-07-16 03:15:27 +0200

    fix: resolve deep ESLint errors, fix pure functions, ignore dist-electron

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'dist-electron']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -14,8 +14,11 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      globals: globals.browser,
+      globals: { ...globals.browser, ...globals.node },
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
+    rules: {
+      "no-unused-vars": "warn"
+    }
   },
 ])
